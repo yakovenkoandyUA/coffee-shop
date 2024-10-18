7b916e411291576d5e429f08979416c8c08b2ce2
Andrey Yakovenko <yakovenkoandyua@gmail.com>
2024-10-18 08:16:33 +0300

    email edit

--- server.js
+++ server.js
@@ -23,20 +23,19 @@ app.use(cors())
 app.use(express.static('./client/'))
 
 export const sendEmail = async (req, res) => {
-	const { name, lastName, desc, mail, toAddress,storage } = req.body
-	console.log(req.body);
+	const { name, lastName, desc, mail, toAddress, storage } = req.body
+	console.log(req.body)
 	const info = await transporter.sendMail({
 		to: mail, // list of receivers
 		subject: `Замовлення`, // Subject line
 		html: `Вітаю шановний/на ${name} ${lastName}, ваше замовлення прийнято в роботу та буде відправлено на протязі декількох днів. Дякую. що обрали саме нас. Гарного дня!
 		`, // html body
-		
 	})
-	const str = ''
+	let str = ''
 	storage.forEach(i => {
-		str += `<p>${i.title}</p><span> ${id}</span>`
+		str += `<p>${i.title}</p><span> ${i.id}</span>`
 	})
-	
+
 	const info1 = await transporter.sendMail({
 		to: 'elitejapanimports1@gmail.com', // list of receivers
 		subject: `Замовлення`, // Subject line
@@ -45,7 +44,7 @@ export const sendEmail = async (req, res) => {
 
 	// console.log('Message sent: %s', info.messageId)
 	// Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
-	res.status(200).send(info.messageId)
+	res.status(200).send({ info: info.messageId, info1: info1.messageId })
 }
 
 // const getStart = async (req, res) => {
