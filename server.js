edbc7bbdbd9f1630e1fc783ba6804b6d8fdff097
Andrey Yakovenko <yakovenkoandyua@gmail.com>
2024-10-22 17:34:16 +0300

    edit car7

--- server.js
+++ server.js
@@ -135,7 +135,7 @@ async function addTasks(req, res) {
 const DB_URL = 'mongodb+srv://yakovenkoandyua:forest548@japan-co.lic0y.mongodb.net/?retryWrites=true&w=majority&appName=Japan-co'
 
 mongoose
-	.connect(process.env.MONGODB_URI || DB_URL, {
+	.connect(process.env.MONGODB_URI, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 	})
